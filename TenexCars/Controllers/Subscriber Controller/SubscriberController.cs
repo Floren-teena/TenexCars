bd8cb9e704384ac9023355a7dab8454108058437
Floren-teena <florentinaantigha27@gmail.com>
2024-10-29 22:37:31 +0100

    Designed View for Fleet of Cars page

--- TenexCars/Controllers/Subscriber Controller/SubscriberController.cs
+++ TenexCars/Controllers/Subscriber Controller/SubscriberController.cs
@@ -1,17 +1,35 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using TenexCars.DataAccess.Repositories.Interfaces;
+using TenexCars.Models.ViewModels;
 
 namespace TenexCars.Controllers.Subscriber_Controller
 {
     public class SubscriberController : Controller
     {
-        public SubscriberController() 
-        {
+        private readonly IVehicleRepository _vehicleRepository;
 
+        public SubscriberController(IVehicleRepository vehicleRepository) 
+        {
+            _vehicleRepository = vehicleRepository;
         }
 
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> ReserveCar(string id)
+        {
+            var vehicle = await _vehicleRepository.GetVehicleById(id);
+
+            var response = new ReserveCarViewModel
+            {
+                VehicleId = vehicle!.Id,
+                ImageUrl = vehicle.ImageUrl,
+            };
+
+            return View(response);
         }
     }
 }
