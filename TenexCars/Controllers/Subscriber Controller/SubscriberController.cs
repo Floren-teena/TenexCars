bc57417c44ff551394edff3d071fd6841c6b3a63
Floren-teena <florentinaantigha27@gmail.com>
2024-10-29 22:09:24 +0100

    Designed View for Reserve Car page

---
+++ TenexCars/Controllers/Subscriber Controller/SubscriberController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace TenexCars.Controllers.Subscriber_Controller
+{
+    public class SubscriberController : Controller
+    {
+        public SubscriberController() 
+        {
+
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
