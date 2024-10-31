18a7dfa7b2e6058a30957bcc5ecaf7996c510a03
Floren-teena <florentinaantigha27@gmail.com>
2024-10-31 14:03:45 +0100

    Designed the view for Operator Dashboard page

---
+++ TenexCars/Models/ViewModels/OperatorDashboardViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace TenexCars.Models.ViewModels
+{
+    public class OperatorDashboardViewModel
+    {
+        public string OperatorId { get; set; } = null!;
+        public string OperatorLogo { get; set; } = null!;
+        public string? CompanyLogo { get; set; }
+        public string OperatorImage { get; set; } = null!;
+        public int TotalNumberOfVehicles { get; set; }
+        public int TotalNumberOfSubscribers { get; set; }
+        public int TotalNumberOfActiveCars { get; set; }
+        public int TotalNumberOfReservedCars { get; set; }
+        public List<int> CurrentMonthStats { get; set; } = null!;
+        public List<int> PastMonthStats { get; set; } = null!;
+    }
+}
