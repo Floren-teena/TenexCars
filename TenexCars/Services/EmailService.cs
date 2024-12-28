9f316f6a2d9dad5070ddbfa4fe0704fe07dfc506
Floren-teena <florentinaantigha27@gmail.com>
2024-12-28 10:46:16 +0100

    Designed the Edit Vehicle page

--- TenexCars/Services/EmailService.cs
+++ TenexCars/Services/EmailService.cs
@@ -1,4 +1,5 @@
 ﻿using MailKit.Net.Smtp;
+using MailKit.Security;
 using MimeKit;
 using TenexCars.DTOs;
 using TenexCars.Interfaces;
@@ -6,6 +7,49 @@ using TenexCars.Interfaces;
 namespace TenexCars.Services
 {
     public class EmailService : IEmailService
+    {
+        private readonly IConfiguration _config;
+
+        public EmailService(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public async Task SendOperatorSetPasswordEmailAsync(EmailDto emailDto)
+        {
+            try
+            {
+                var message = new MimeMessage();
+                message.From.Add(new MailboxAddress("Tenex Cars", _config["EmailSettings:Username"]));
+                message.To.Add(new MailboxAddress("", emailDto.To)); // Ensure 'emailDto.To' is valid
+                message.Subject = emailDto.Subject;
+                message.Body = new TextPart("html")
+                {
+                    Text = emailDto.Body // Ensure 'emailDto.Body' is valid HTML
+                };
+
+                using (var client = new SmtpClient())
+                {
+                    client.ServerCertificateValidationCallback = (s, c, h, e) => true; // Remove this for production
+                    await client.ConnectAsync(_config["EmailSettings:Host"], int.Parse(_config["EmailSettings:Port"]!), SecureSocketOptions.StartTls);
+                    await client.AuthenticateAsync(_config["EmailSettings:Username"], _config["EmailSettings:Password"]);
+                    await client.SendAsync(message);
+                    await client.DisconnectAsync(true);
+                }
+            }
+            catch (SmtpCommandException ex)
+            {
+                Console.WriteLine($"SMTP Command Exception: {ex.Message}, Status Code: {ex.StatusCode}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred while sending email: {ex.Message}");
+            }
+        }
+    }
+
+
+    /*public class EmailService : IEmailService
     {
         private readonly IConfiguration _config;
 
@@ -41,5 +85,5 @@ namespace TenexCars.Services
                 Console.WriteLine($"An error occurred while sending email: {ex.Message}");
             }
         }
-    }
+    }*/
 }
