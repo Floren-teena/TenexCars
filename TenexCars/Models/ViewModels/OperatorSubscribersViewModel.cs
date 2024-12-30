ad934a0848acf62e5beb31bec45c7e426ba20a78
Floren-teena <florentinaantigha27@gmail.com>
2024-12-30 13:32:17 +0100

    Designed the view for Operator subscribers

---
+++ TenexCars/Models/ViewModels/OperatorSubscribersViewModel.cs
@@ -0,0 +1,10 @@
+﻿using TenexCars.DataAccess.Models;
+
+namespace TenexCars.Models.ViewModels
+{
+    public class OperatorSubscribersViewModel
+    {
+        public Operator? Operator { get; set; }
+        public IEnumerable<Subscription>? Subscriptions { get; set; }
+    }
+}
